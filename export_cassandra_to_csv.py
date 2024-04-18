ec0f1a0e313598b1a7840ce9b5baec303f52fb51
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-04-19 00:29:53 +0400

    Updated in 'Ajman City, AE' on BRANCH 'project-3-thalia' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:

---
+++ export_cassandra_to_csv.py
@@ -0,0 +1,27 @@
+import subprocess
+
+
+cassandra_host = '172.27.89.125'
+cassandra_port = 9042
+keyspace = 'cs2_keyspace_5'
+tables = [ 'cs2_games', 'cs2_players']
+
+output_dir = 'output'
+
+def export_table_to_csv(keyspace, table, output_dir):
+    # Construct the COPY command
+    copy_command = f"cqlsh {cassandra_host} {cassandra_port} -e \"COPY {keyspace}.{table} TO '{output_dir}/{table}.csv' WITH HEADER = true;\""
+    print(copy_command)
+
+    # Execute the COPY command
+    subprocess.run(copy_command, shell=True, check=True)
+
+# Main function
+def main():
+
+    for table in tables:
+        export_table_to_csv(keyspace, table, output_dir)
+        print(f"Exporting {table} to CSV...")
+
+if __name__ == "__main__":
+    main()
