f7c42bb7d057d09d93ab487e9847cef773fc4af7
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-04-21 01:00:23 +0400

    Updated in 'Abu Dhabi, AE' on BRANCH 'project-3-thalia' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  set_env.sh starter/dbt_transformation/models/staging/src_big_star_db.yml

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export DBT_BIGQUERY_DATASET=transformed_data
-export DBT_BIGQUERY_KEYFILE_PATH=/Users/ahmed.soliman/workspace/ahmed/personal/data-eng/big-star-420419-61bab6eb8c72.json
-export DBT_BIGQUERY_LOCATION=US
-export DBT_BIGQUERY_PROJECT=big-star-420419
-export DBT_PROFILES_DIR=/Users/ahmed.soliman/workspace/ahmed/personal/data-eng/starter/dbt_transformation/config
+# export DBT_BIGQUERY_DATASET=transformed_data
+# export DBT_BIGQUERY_KEYFILE_PATH=/Users/ahmed.soliman/workspace/ahmed/personal/data-eng/big-star-420419-5b89f692710d.json
+# export DBT_BIGQUERY_LOCATION=US
+# export DBT_BIGQUERY_PROJECT=big-star-420419
+# export DBT_PROFILES_DIR=/Users/ahmed.soliman/workspace/ahmed/personal/data-eng/starter/dbt_transformation/config
 
 echo "---------"
 echo $DBT_BIGQUERY_DATASET
